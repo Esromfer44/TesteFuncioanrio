2f2c57181701d7348b8cf66b9156e7577b2593e8
aluno <aluno@udicentro.local>
2023-04-13 20:01:19 -0300

    Initial commit

---
+++ src/main/java/Funcionario.java
@@ -0,0 +1,46 @@
+public class Funcionario {
+    private String nome;
+    private int horasTrabalhadas;
+    private double valorHora;
+
+    public Funcionario(String nome, int horasTrabalhadas, double valorHora) {
+        this.nome = nome;
+        this.horasTrabalhadas = horasTrabalhadas;
+        this.valorHora = valorHora;
+    }
+
+    public double calculaPagamento() {
+        double salarioMinimo = 1320.0;
+        double valorPagamento = horasTrabalhadas * valorHora;
+
+        if (valorPagamento < salarioMinimo) {
+            valorPagamento = salarioMinimo;
+        }
+
+        return valorPagamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getHorasTrabalhadas() {
+        return horasTrabalhadas;
+    }
+
+    public void setHorasTrabalhadas(int horasTrabalhadas) {
+        this.horasTrabalhadas = horasTrabalhadas;
+    }
+
+    public double getValorHora() {
+        return valorHora;
+    }
+
+    public void setValorHora(double valorHora) {
+        this.valorHora = valorHora;
+    }
+}
---
+++ src/test/java/Trabalho/Atv03/Atv03ApplicationTests.java
@@ -0,0 +1,13 @@
+package Trabalho.Atv03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atv03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
